04f0c394a0e49ac353d4b22a45bac68f69be8fde
Jonathan <jonathan.fauchoux@gmail.com>
2018-11-10 19:38:00 +0100

    bug fixed

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    // Transition effect for navbar 
-    $(window).scroll(function() {
-      // checks if window is scrolled more than 500px, adds/removes solid class
+    $(window).scroll(function() {// checks if window is scrolled more than 500px, adds/removes solid class
       if($(this).scrollTop() > 200) { 
           $('.navglobal').addClass('solid');
           $('.collapse').addClass('solid');
