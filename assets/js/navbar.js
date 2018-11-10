c9be3333a045d084e4f6eae010d616bbd187544b
Jonathan <jonathan.fauchoux@gmail.com>
2018-11-10 12:12:15 +0100

    bugfix + navbar opacity

---
+++ assets/js/navbar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 200) { 
+          $('.navglobal').addClass('solid');
+          $('.collapse').addClass('solid');
+      } else {
+          $('.navglobal').removeClass('solid');
+          $('.collapse').removeClass('solid');
+      }
+    });
+});
