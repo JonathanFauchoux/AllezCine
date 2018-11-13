d912832f431037632449d592d59a989ad3e0df7a
rubenpereirac <rubenpereira.work@gmail.com>
2018-11-13 12:26:59 +0100

    ajout formulaire-modal

---
+++ assets/js/modal-form.js
@@ -0,0 +1,28 @@
+let nom = $("#exampleInputEmail1").val();
+let tel = $("#exampleInputPassword1").val();
+let prenom = $("#exampleInputEmail2").val();
+let email = $("#exampleInputPassword2").val();
+let usertexte = $("#exampleFormControlTextarea1").val();
+let result = "Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte;
+
+
+$( "#send-message").click( function() {
+
+
+    $( "#modal-form" ).dialog({
+        resizable: true,
+        height: "auto",
+        width: 600,
+        modal: true,
+        buttons: {
+            "cancel": function(){
+                $(this).dialog("close");
+            }
+        },
+    
+    });
+$("#modal-form").text(result);
+    //$("#modal-form").text("Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte);
+
+
+});
