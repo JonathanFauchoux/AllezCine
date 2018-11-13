cd4103255af8ce4a1769f7debc2d99dde7fbc795
rubenpereirac <rubenpereira.work@gmail.com>
2018-11-13 15:19:24 +0100

    Ajout du modal-formulaire

--- assets/js/modal-form.js
+++ assets/js/modal-form.js
@@ -1,13 +1,13 @@
-let nom = $("#exampleInputEmail1").val();
-let tel = $("#exampleInputPassword1").val();
-let prenom = $("#exampleInputEmail2").val();
-let email = $("#exampleInputPassword2").val();
-let usertexte = $("#exampleFormControlTextarea1").val();
-let result = "Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte;
+
+//let result = "Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte;
 
 
 $( "#send-message").click( function() {
-
+    let nom = $("#exampleInputEmail1").val();
+    let tel = $("#exampleInputPassword1").val();
+    let prenom = $("#exampleInputEmail2").val();
+    let email = $("#exampleInputPassword2").val();
+    let usertexte = $("#exampleFormControlTextarea1").val();
 
     $( "#modal-form" ).dialog({
         resizable: true,
@@ -17,12 +17,13 @@ $( "#send-message").click( function() {
         buttons: {
             "cancel": function(){
                 $(this).dialog("close");
+                $(this).addClass("btn-mod");
             }
         },
     
     });
-$("#modal-form").text(result);
-    //$("#modal-form").text("Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte);
+    //$("#modal-form").text(result);
+    $("#modal-form").text("Votre nom est : " + nom + "\n" + " Votre prenom : " + prenom + "\n" + " Votre numéro : " + tel + "\n" + " Votre e-mail : " + email + "\n" + " Votre texte : " + usertexte);
 
 
 });
